e520bd08049d08d9280575d02985acb3f11029e7
Michal Puškel <michal.puskel@ui42.sk>
2016-02-20 22:47:37 +0100

    @master

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    /**
+     * @author Michal_Puškel
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Java!");
+    }
+
+}
