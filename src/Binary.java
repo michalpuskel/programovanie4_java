03cb3314188ac9899daa7156f6820379b6d416f2
Michal Puškel <michal.puskel@ui42.sk>
2016-02-21 15:26:12 +0100

    @1 Binarny zapis cisla

--- src/Binary.java
+++ src/Binary.java
@@ -1,3 +1,32 @@
-
+/**
+ * @author misko
+ */
 public class Binary {
+
+    public static void main(String[] args) {
+
+        System.out.println(pocetJednotiek(10) + "\n");
+        System.out.println(pocetJednotiek(65535) + "\n");
+        System.out.println(pocetJednotiek(15) + "\n");
+
+    }
+
+    /**
+     * funkcia spocita kolkokrat sa da celociselne vydelit cislo 2kou
+     * @param n vstupne cislo v desiatkovej sustave
+     * @return
+     */
+    public static long pocetJednotiek(int n) {
+        System.out.printf("%-8d", n);
+        long poc = 0;
+        String bin = "";
+        while (n > 0) {
+            bin = n % 2 + bin;
+            poc += n % 2;
+            n /= 2;
+        }
+        System.out.println(" = " + bin);
+        return poc;
+    }
+
 }
